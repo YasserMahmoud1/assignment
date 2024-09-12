b8e1302492b11d6bef57d8c39416fcfec639f5b5
YasserMahmoud1 <109506170+YasserMahmoud1@users.noreply.github.com>
2024-09-12 21:37:23 +0300

    update

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -7,16 +7,22 @@ class APIService {
   static const _apiKey = '2f8f8d6f2068c7fe7832805515f2cdbb';
 
   static Future<List<Movie>> getPopularMovies() async {
+    final uri = Uri.https('https://api.themoviedb.org/3/movie/top_rated??api_key=$_apiKey');
+    final r = await http.get(uri);
+    final response = jsonDecode(r.body) as Map<String, dynamic>;
+    return response["results"].map<Movie>((movie) => Movie.fromJSON(movie)).toList();
+  }
+  static Future<List<Movie>> getTrendingMovies() async {
     final uri = Uri.https('https://api.themoviedb.org/3/trending/movie/week?api_key=$_apiKey');
     final r = await http.get(uri);
     final response = jsonDecode(r.body) as Map<String, dynamic>;
     return response["results"].map<Movie>((movie) => Movie.fromJSON(movie)).toList();
   }
-  // final Dio _dio;
-  // DioHelper(this._dio);
+  static Future<List<Movie>> getNowInTheaterMovies() async {
+    final uri = Uri.https('https://api.themoviedb.org/3/movie/now_playing?api_key=$_apiKey');
+    final r = await http.get(uri);
+    final response = jsonDecode(r.body) as Map<String, dynamic>;
+    return response["results"].map<Movie>((movie) => Movie.fromJSON(movie)).toList();
+  }
 
-  // Future<Map<String, dynamic>> get({required String endPoint}) async {
-  //   var response = await _dio.get('$_baseUrl${endPoint}api_key=$_apiKey');
-  //   return response.data;
-  // }
 }
