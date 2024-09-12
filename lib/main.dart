304a351f8a918e8cbdc97cd83d0069a130986361
YasserMahmoud1 <109506170+YasserMahmoud1@users.noreply.github.com>
2024-09-12 22:17:48 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
